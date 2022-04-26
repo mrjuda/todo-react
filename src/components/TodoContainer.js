81d485b5ea59facad1ae4e0c9407ff458452db31
judah <87juda@gmail.com>
2022-04-26 20:14:18 -0300

    Add component files

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,16 @@
+// TodoContainer.js
+
+import React from "react";
+
+class TodoContainer extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello from Create React App Galadim</h1>
+        <p>I am in a React Component! Reiosse!</p>
+      </React.Fragment>
+    )
+  }
+}
+
+export default TodoContainer;
